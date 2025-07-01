2c316e0c82497a52caedac45dd1a3c41af3bc541
Raskolnikov <jerry.tann@endress.com>
2025-07-01 16:11:49 +0800

    🚧 post

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app  = Flask("Try Flask")
 
@@ -15,6 +16,13 @@ def about():
 @app.route('/user/<name>')
 def user(name):
    return render_template('index.html', name=name)
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit() :
+   if request.method == 'POST':
+      data = request.form['input_name']
+      return f'You submitted: {data}'
+   return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
